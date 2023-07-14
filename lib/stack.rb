91b850f6f73c74566ffa1c00b84d0136060393db
sjamescarter <stephenjamescarter@gmail.com>
2023-07-14 17:00:40 -0500

    commit tree 71d99a919d4baa8a05492a4ecf987fd49533ae42

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,49 @@
-# your code here
+class Stack
+  def initialize(limit=nil)
+    @stack= []
+    @limit= limit
+  end
+
+  def push(value)
+    if @limit
+      @stack.length < @limit ? @stack.push(value) : raise_error
+    else
+      @stack.push(value)
+    end
+  end
+
+  def pop
+    @stack.pop
+  end   
+
+  def peek
+    @stack[-1]
+  end
+
+  def size
+    @stack.length
+  end
+
+  def empty?
+    @stack.empty?
+  end
+
+  def full?
+    @stack.length == @limit ? true : false
+  end
+
+  def search(value)
+    @stack.each do |v| 
+      if v == value 
+        return @stack.length - 1 - @stack.index(v)
+      end
+    end
+    -1
+  end
+
+  private
+
+  def raise_error
+    raise Exception.new 'Stack Overflow'
+  end
+end
